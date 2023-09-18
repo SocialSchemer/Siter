58057b2db31b1bbffd992e2362ad648455ff6f2e
SocialSchemer <143866912+SocialSchemer@users.noreply.github.com>
2023-09-18 11:07:48 +0300

    Add files via upload
    A cool business card site for a developer, the ability to edit, as well as the most pleasant, clear and beautiful code
    <3

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var message=(message="SocialSchemer | Главная")+" ";i="0";var temptitle="",speed="200";function titler(){(document.all||document.getElementById)&&(document.title=temptitle+message.charAt(i),temptitle+=message.charAt(i),i++,i==message.length&&(i="0",temptitle=""),setTimeout("titler()",speed))}window.onload=titler;
